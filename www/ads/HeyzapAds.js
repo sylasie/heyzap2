f6d43535df0a2cf98ccd29c9903320362612d2c5
sylasie <nowak.magda93@gmail.com>
2017-04-19 17:03:17 +0200

    Done

--- www/ads/HeyzapAds.js
+++ www/ads/HeyzapAds.js
@@ -180,7 +180,7 @@
 
     IncentivizedAd: cordova.require('heyzap-cordova.ads.IncentivizedAd'),
 
-    OfferWall : cordova.require('heyzap-cordova.ads.OfferWall')
+    OfferWall : cordova.require('heyzap-cordova.ads.OfferWall'),
 
     BannerAd: cordova.require('heyzap-cordova.ads.BannerAd')
   };
